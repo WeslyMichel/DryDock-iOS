183c7c645edbac9226cf0b1afe3b002bde45438b
WeslyMichel <michel.wesly@gmail.com>
2015-04-07 19:46:55 -0700

    Testing

--- VenmoDryDock/VDDAppDelegate.m
+++ VenmoDryDock/VDDAppDelegate.m
@@ -21,45 +21,14 @@
 // THE SOFTWARE.
 
 #import "VDDAppDelegate.h"
-#import <VENVersionTracker/VENVersionTracker.h>
 #import "VDDConstants.h"
 
 @implementation VDDAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [Parse setApplicationId:VDDParseAppId
-                  clientKey:VDDParseClientKey];
-    
-    [self startTrackingVersion];
-    
+                  clientKey:VDDParseClientKey];    
     return YES;
-}
-
-
-- (void)startTrackingVersion {
-    // For details on using VENVersionTracker see -- https://github.com/venmo/VENVersionTracker
-    /*
-     The URL http://YOUR_BASE_URL/track/YOUR_CHANNEL_NAME should return the following JSON:
-
-     {
-        "version":{
-            "number":"0.1.0",
-            "mandatory":false,
-            "install_url":"<<ITMS INSTALL URL>>"
-        },
-        "min-version-number":0.1.0
-     }
-     */
-    [VENVersionTracker beginTrackingVersionForChannel:VDDChannelName
-                                       serviceBaseUrl:VDDBaseUrl
-                                         timeInterval:1800
-                                          withHandler:^(VENVersionTrackerState state, VENVersion *version) {
-                                              dispatch_sync(dispatch_get_main_queue(), ^{
-                                                  if (state == VENVersionTrackerStateDeprecated || state == VENVersionTrackerStateOutdated) {
-                                                      [version install];
-                                                  }
-                                              });
-                                          }];
 }
 
 @end
